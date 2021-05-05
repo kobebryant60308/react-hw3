174db26ed63e64b1e2fc352aa9393ceed99bcce4
kobebryant60308 <kobebryant60308@gmail.com>
2021-05-05 15:58:39 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import SignInSide from './SignInSide.js';
+
+
+ function App() {
+  return (
+    <div className="App">
+      <div>
+      {SignInSide()}
+      </div>
+    </div>
+
+  );
+ }
+
+
+export default App;
